ce53291786d9e21e096392db6397e41097ad9285
Ryan Park <22parkr@millburn.org>
2019-10-17 22:35:00 -0400

    Update train params in mnist.py

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,10 +1,16 @@
+# getting stuff from easyai
 from easyai import NN
 from easyai.layers import Input, FC
 from easyai.support.datasets import Builtins
 
+# loads mnist dataset
 (x_train, y_train), (x_test, y_test) = Builtins.load_mnist(mode="mlp")
 
-mlp = NN(Input(784), FC(100), FC(10))
+# creates model
+nn = NN(Input(784), FC(100), FC(10))
 
-mlp.train(x_train, y_train)
-mlp.evaluate(x_test, y_test)
+# trains model
+nn.train(x_train, y_train, epochs=3)
+
+# tests model on different data than training
+nn.evaluate(x_test, y_test)

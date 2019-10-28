0bb488120a0b65a952a7684af3d9aabc871e494f
Ryan Park <22parkr@millburn.org>
2019-10-28 09:58:26 -0400

    updating mnist.py to include interactive

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -2,6 +2,8 @@
 from easyai import NN
 from easyai.layers import Input, FC
 from easyai.support.datasets import Builtins
+from easyai.support.draw import Draw
+from tkinter import *
 
 # loads mnist dataset
 (x_train, y_train), (x_test, y_test) = Builtins.load_mnist(mode="mlp")
@@ -14,3 +16,8 @@ nn.train(x_train, y_train, epochs=3)
 
 # tests model on different data than training
 nn.evaluate(x_test, y_test)
+
+# interactive digit classifier!
+root = Tk()
+Draw(root, model)
+Draw.run_root(root, title="MNIST")
